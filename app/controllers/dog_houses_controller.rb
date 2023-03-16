8a1be9b8c4db2198f72da4617d573112c0885dfd
margaret manyeki <maggiemanyeki6@gmail.com>
2023-03-16 17:59:06 +0300

    added sln

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,15 +1,16 @@
-class DogHousesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
-  def show
-    dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+ class DogHousesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+    def show
+      dog_house = DogHouse.find(params[:id])
+      render json: dog_house
+    end
+  
+    private
+  
+    def render_not_found_response
+      render json: { error: "Dog house not found" }, status: :not_found
+    end
+  
   end
-
-  private
-
-  def render_not_found_response
-    render json: { error: "Dog house not found" }, status: :not_found
-  end
-
-end
+  
